97425d0e562aa311e20ad988cb91db199ee7fc54
unknown <you@example.com>
2024-04-06 02:57:11 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 function App() {
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  const [timerId, setTimerId] = useState();
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      
+		.then((res) =>
+        setTimerId(() => {
+          setTimeout(() => {
+            clearTimeout(timerId);
+            setData(
+              res.users.filter((e) =>
+                e.firstName
+                  .trim()
+                  .toLowerCase()
+                  .startsWith(text.trim().toLowerCase())
+              )
+            );
+          }, 1000);
+        })
+      );
+  }, [text]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(evt) => setText(evt.target.value)} />
+      <div className="cards">
+        {data.map((e) => {
+          return (
+            <>
+              <div className="card">
+                <img src={e.image} />
+                <h3>{e.firstName}</h3>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
